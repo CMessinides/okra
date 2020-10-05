78896bbab083f048751f42c4b328d12f14847f94
Cameron Messinides <cmessinides@tammantech.com>
2020-10-05 17:51:43 -0400

    Dump tokens in CLI

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { readFileSync } = require("fs");
-const { parse } = require("./dist/parser");
+const { scan } = require("./dist/scanner");
 
 if (process.argv[2] === undefined) {
 	console.error("ERROR: No input file provided.");
@@ -16,14 +16,48 @@ if (process.argv[2] === undefined) {
 		process.exit(1);
 	}
 
-	let { ok, ast, errors } = parse(data);
-	if (!ok) {
-		console.error("ERROR: Encountered errors while parsing:");
-		for (const error of errors) {
-			console.error(error);
-		}
-		process.exit(1);
-	} else {
-		console.log(ast);
+	let tokens = scan(data);
+	let rows = tokens.map(({ type, lexeme, loc }) => [
+		loc.offset,
+		type,
+		JSON.stringify(lexeme),
+		`${loc.line}:${loc.col}`,
+	]);
+	let lines = padAll(rows);
+	console.log("--- [ TOKENS ] ---");
+	for (const line of lines) {
+		console.log(line);
 	}
+	console.log("------------------");
+}
+
+/**
+ *
+ * @param {(string|number)[][]} rows
+ */
+function padAll(rows) {
+	let columnMaxes = [];
+
+	for (const row of rows) {
+		for (let i = 0; i < row.length; i++) {
+			let length = row[i].toString().length;
+			let currentMax = columnMaxes[i] || 0;
+			if (currentMax < length) {
+				columnMaxes[i] = length;
+			}
+		}
+	}
+
+	return rows.map((row) =>
+		row
+			.map((cell, i) => {
+				let max = columnMaxes[i];
+				if (typeof cell === "number") {
+					return cell.toString().padStart(max, "0");
+				} else {
+					return cell.toString().padEnd(max, " ");
+				}
+			})
+			.join(" ".repeat(4))
+	);
 }

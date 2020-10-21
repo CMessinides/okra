6443ef120d5217c2c1ca65ba80068bdc63b2909e
Cameron Messinides <cmessinides@tammantech.com>
2020-10-20 23:58:22 -0400

    Remove old pretty print cruft

--- src/bin/lib/fmt.ts
+++ src/bin/lib/fmt.ts
@@ -1,25 +1,5 @@
-import * as path from "path";
 import chalk from "chalk";
-import { SyntaxError } from "../../ast";
-import { Token } from "../../tokens";
-import { prettyPrintError } from "../../printer";
 
 export function error(e: Error) {
 	return `${chalk.bold.red("ERROR")}: ${e.message}`;
-}
-
-export function parseError(
-	error: SyntaxError,
-	source: string,
-	tokens: Token[],
-	filepath: string
-) {
-	let { loc } = error.token;
-
-	return [
-		`${chalk.bold.cyan(path.relative(process.cwd(), filepath))}:${chalk.yellow(
-			loc.line + ":" + loc.col
-		)} - ${chalk.red("error")} - ${error.message}`,
-		prettyPrintError(error, source, tokens),
-	].join("\n\n");
 }
